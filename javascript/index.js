b6a6ccdd5bc88a455d98fc689b6631859b5c81aa
fredymontalvo <fredualdo@gmail.com>
2024-02-25 20:03:04 +0100

    Fixed the logo appearing in the app version

--- javascript/index.js
+++ javascript/index.js
@@ -1,8 +1,10 @@
 const hamburguer = document.querySelector(".hamburguer");
 const menu = document.querySelector(".menu-navegacion");
+const logo = document.querySelector(".logo-herosection"); // Agrega esta línea para seleccionar el logo
 
 hamburguer.addEventListener("click", () => {
   menu.classList.toggle("spread");
+  logo.classList.toggle("hide"); // Agrega o quita la clase para ocultar el logo
 });
 
 window.addEventListener("click", (e) => {
@@ -12,5 +14,6 @@ window.addEventListener("click", (e) => {
     e.target != hamburguer
   ) {
     menu.classList.toggle("spread");
+    logo.classList.toggle("hide"); // Asegúrate de ocultar el logo si se hace clic fuera del menú
   }
 });
